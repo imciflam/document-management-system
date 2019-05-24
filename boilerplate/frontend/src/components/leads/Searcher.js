db5e81cab61df21ae29c03b766e926785cbb09f7
Vita <vitalia@myrambler.ru>
2019-05-24 21:02:22 +0300

    + event handler. + filtering

--- boilerplate/frontend/src/components/leads/Searcher.js
+++ boilerplate/frontend/src/components/leads/Searcher.js
@@ -5,35 +5,37 @@ export class Searcher extends Component {
     term: "",
     data: this.props.leadsFromParent
   };
-  dataSearch = e => {
-    const value = e.target.value.toLowerCase();
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      data: this.props.leadsFromParent
+    };
+  }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    const value = e.target.value.toLowerCase();
+    console.log(value);
     const filter = this.props.leadsFromParent.filter(user => {
       return user.name.toLowerCase().includes(value);
     });
     console.log(filter);
-    // update({
-    //   data: filter,
-    //   active: 0,
-    //   term: value
-    // });
-    this.setState({
-      [e.target.name]: e.target.value
-    });
   };
 
   render() {
+    const { term, data } = this.state;
     console.log(this.props.leadsFromParent);
-    const { term } = this.state;
     return (
       <div>
         <div className="searchbar form-group">
           <input
             value={term}
             type="text"
+            name="term"
             className="form-control"
             placeholder="Поиск по предмету записки..."
-            onChange={this.dataSearch}
+            onChange={this.onChange}
           />
         </div>
       </div>
