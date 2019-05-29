1351b312f29033e81417cdedd19ec40fe37e6c09
Vita <vitalia@myrambler.ru>
2019-05-29 20:07:54 +0300

    for postgres module

--- boilerplate/boilerplate/settings.py
+++ boilerplate/boilerplate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'frontend',
     'knox',
     'accounts',
-    'treaties'
+    'treaties',
+    'django_heroku'
 ]
 
 REST_FRAMEWORK = {
@@ -131,3 +133,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'fierce-spire-55675.herokuapp.com', 'localhost']
+
+django_heroku.settings(locals())
